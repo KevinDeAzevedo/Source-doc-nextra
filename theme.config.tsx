983acdf520fffecadadbad5e751e12a976733d43
KevinDeAzevedo <deazevedo.kevin@gmail.com>
2024-01-21 10:45:33 +0100

    add logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,70 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <>
+    <svg width="150" height="100" viewBox="0 0 677 132" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clip-path="url(#clip0_237_1311)">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M89.9847 131.155C139.707 131.118 180 101.772 180 65.5775C180 29.3601 139.657 -4.35702e-06 89.8913 -3.93877e-06C59.479 -5.26814e-06 34.8249 20.6918 34.8249 46.2165C34.8249 53.1298 36.6335 59.6885 39.8751 65.5775C48.6021 49.7232 67.716 38.722 89.8913 38.722C120.304 38.722 144.958 59.4138 144.958 84.9385C144.958 110.437 120.354 131.113 89.9847 131.155Z" fill="url(#paint0_linear_237_1311)" />
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M89.9945 0.09454C40.2813 0.13941 1.33452e-05 29.4822 8.59945e-06 65.672C3.85011e-06 101.889 40.343 131.25 90.1087 131.25C120.521 131.25 145.175 110.558 145.175 85.033C145.175 78.1198 143.367 71.5611 140.125 65.672C131.398 81.5263 112.284 92.5275 90.1087 92.5275C59.6964 92.5275 35.0423 71.8357 35.0423 46.311C35.0423 20.8182 59.6347 0.146218 89.9945 0.09454Z" fill="url(#paint1_linear_237_1311)" />
+        <path d="M277.125 48.0173L268.116 50.567C267.549 49.0655 266.714 47.6065 265.609 46.19C264.532 44.7451 263.059 43.5552 261.189 42.6203C259.319 41.6854 256.926 41.218 254.007 41.218C250.013 41.218 246.684 42.1387 244.021 43.9802C241.386 45.7933 240.069 48.1023 240.069 50.907C240.069 53.4001 240.975 55.3691 242.789 56.8139C244.602 58.2588 247.435 59.4628 251.288 60.4261L260.977 62.8058C266.813 64.2224 271.162 66.3897 274.023 69.3077C276.884 72.1974 278.315 75.9229 278.315 80.4841C278.315 84.2238 277.239 87.5668 275.085 90.5131C272.961 93.4595 269.986 95.7826 266.161 97.4825C262.337 99.1823 257.889 100.032 252.818 100.032C246.16 100.032 240.65 98.5873 236.287 95.6976C231.924 92.8079 229.162 88.5867 228 83.0339L237.519 80.6541C238.426 84.1671 240.14 86.8018 242.661 88.5583C245.211 90.3148 248.54 91.1931 252.648 91.1931C257.322 91.1931 261.033 90.2015 263.781 88.2184C266.558 86.2069 267.946 83.7988 267.946 80.9941C267.946 78.7276 267.153 76.8295 265.566 75.2996C263.98 73.7414 261.543 72.5799 258.257 71.815L247.378 69.2652C241.4 67.8487 237.009 65.6531 234.204 62.6784C231.428 59.6753 230.04 55.9215 230.04 51.417C230.04 47.734 231.074 44.476 233.142 41.6429C235.238 38.8099 238.086 36.5859 241.684 34.9711C245.31 33.3562 249.418 32.5488 254.007 32.5488C260.467 32.5488 265.538 33.9653 269.221 36.7984C272.932 39.6314 275.567 43.3711 277.125 48.0173Z" fill="url(#paint2_linear_237_1311)" />
+        <path d="M337.382 90.8939C351.05 90.8939 362.162 79.7818 362.162 66.114C362.162 52.4462 351.05 41.3341 337.382 41.3341C323.715 41.3341 312.603 52.4462 312.603 66.114C312.603 79.7818 323.715 90.8939 337.382 90.8939ZM303.38 66.114C303.38 47.3346 318.603 32 337.382 32C356.273 32 371.496 47.3346 371.496 66.114C371.496 84.8934 356.273 100.228 337.382 100.228C318.603 100.228 303.38 84.8934 303.38 66.114Z" fill="url(#paint3_linear_237_1311)" />
+        <path d="M402.008 33.6668H411.231V73.8925C411.231 83.3377 418.898 91.005 428.343 91.005C437.677 91.005 445.345 83.3377 445.345 73.8925V33.6668H454.679V73.8925C454.679 88.4492 442.789 100.228 428.343 100.228C413.787 100.228 402.008 88.4492 402.008 73.8925V33.6668Z" fill="url(#paint4_linear_237_1311)" />
+        <path d="M503.228 75.0037H485.227V65.6695H507.228C513.451 65.6695 518.674 60.558 518.674 54.3352C518.674 48.1125 513.451 42.8898 507.228 42.8898H485.227V33.6668H507.228C518.563 33.6668 527.897 43.0009 527.897 54.3352C527.897 62.2248 523.341 69.5588 516.118 73.0035L513.562 74.2258L528.23 98.6723H517.452L503.228 75.0037Z" fill="url(#paint5_linear_237_1311)" />
+        <path d="M555.674 66.114C555.674 48.2236 570.342 33.5557 588.232 33.5557H600.677V42.8898H588.232C575.453 42.8898 564.897 53.3351 564.897 66.114C564.897 78.8929 575.453 89.3382 588.232 89.3382H600.677V98.6723H588.232C570.342 98.6723 555.674 84.0044 555.674 66.114Z" fill="url(#paint6_linear_237_1311)" />
+        <path d="M631.263 70.7811V61.5581H676.267V70.7811H631.263ZM631.263 42.8898V33.6668H676.267V42.8898H631.263ZM676.267 89.4493V98.6723H631.263V89.4493H676.267Z" fill="url(#paint7_linear_237_1311)" />
+      </g>
+      <defs>
+        <linearGradient id="paint0_linear_237_1311" x1="163.105" y1="96.1803" x2="34.9119" y2="36.9724" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#AD5BF7" />
+          <stop offset="1" stop-color="#1553F6" />
+        </linearGradient>
+        <linearGradient id="paint1_linear_237_1311" x1="18.4598" y1="33.8201" x2="164.206" y2="81.7648" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#F1EF6F" />
+          <stop offset="1" stop-color="#FF8BDF" />
+        </linearGradient>
+        <linearGradient id="paint2_linear_237_1311" x1="228.38" y1="100" x2="676.38" y2="32" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#F1EF6F" />
+          <stop offset="0.328125" stop-color="#FF8BDF" />
+          <stop offset="0.6875" stop-color="#AD5BF7" />
+          <stop offset="1" stop-color="#1553F6" />
+        </linearGradient>
+        <linearGradient id="paint3_linear_237_1311" x1="228.38" y1="100" x2="676.38" y2="32" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#F1EF6F" />
+          <stop offset="0.328125" stop-color="#FF8BDF" />
+          <stop offset="0.6875" stop-color="#AD5BF7" />
+          <stop offset="1" stop-color="#1553F6" />
+        </linearGradient>
+        <linearGradient id="paint4_linear_237_1311" x1="228.38" y1="100" x2="676.38" y2="32" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#F1EF6F" />
+          <stop offset="0.328125" stop-color="#FF8BDF" />
+          <stop offset="0.6875" stop-color="#AD5BF7" />
+          <stop offset="1" stop-color="#1553F6" />
+        </linearGradient>
+        <linearGradient id="paint5_linear_237_1311" x1="228.38" y1="100" x2="676.38" y2="32" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#F1EF6F" />
+          <stop offset="0.328125" stop-color="#FF8BDF" />
+          <stop offset="0.6875" stop-color="#AD5BF7" />
+          <stop offset="1" stop-color="#1553F6" />
+        </linearGradient>
+        <linearGradient id="paint6_linear_237_1311" x1="228.38" y1="100" x2="676.38" y2="32" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#F1EF6F" />
+          <stop offset="0.328125" stop-color="#FF8BDF" />
+          <stop offset="0.6875" stop-color="#AD5BF7" />
+          <stop offset="1" stop-color="#1553F6" />
+        </linearGradient>
+        <linearGradient id="paint7_linear_237_1311" x1="228.38" y1="100" x2="676.38" y2="32" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#F1EF6F" />
+          <stop offset="0.328125" stop-color="#FF8BDF" />
+          <stop offset="0.6875" stop-color="#AD5BF7" />
+          <stop offset="1" stop-color="#1553F6" />
+        </linearGradient>
+        <clipPath id="clip0_237_1311">
+          <rect width="677" height="132" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+
+  </>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
@@ -11,7 +74,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Source documentation v.0.1',
   },
 }
 
